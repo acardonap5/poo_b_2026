fa05cf27d2103aca259ebc7fd94fd62b402fa6f2
acardonap5 <acardonap5@miug.edu.gt>
2026-02-22 09:26:42 -0600

    se agrego los get y set

--- Cliente.cpp
+++ Cliente.cpp
@@ -15,6 +15,45 @@ class Cliente : Persona {
 	Cliente(string nom,string ape,string dir,int tel,string fn,string n) : Persona(nom,ape,dir,tel,fn){
 		nit = n;
 	}
+	// set (modificar atributos)
+	void setNit(string n){
+		nit = n;
+	}
+	void setNombres(string nom){
+		nombres = nom;
+	}
+	void setApellidos(string ape){
+		apellidos = ape;
+	}
+	void setDireccion(string dir){
+		direccion = dir;
+	}
+	void setTelefono(int tel){
+		telefono = tel;
+	}
+	void setFechaNacimiento(string fn){
+		fecha_nacimiento = fn;
+	}
+	
+	//get (obtener o visualizar los atributos)
+	string getNit(){
+		return nit;
+	}
+	string getNombres(){
+		return nombres;
+	}
+	string getApellidos(){
+		return apellidos;
+	}
+	string getDireccion(){
+		return direccion;
+	}
+	int getTelefono(){
+		return telefono;
+	}
+	string getFechaNacimiento(){
+		return fecha_nacimiento;
+	}
 	// Metodos
 	void leer(){
 		cout<<"Nit:"<<nit<<endl;
